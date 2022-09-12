27ab3b6d1a7b2e6bc03d496de0302bb85555b8ee
FatimaKabalan <kabalan.fatima01@gmail.com>
2022-09-12 23:55:57 +0300

    fixing the function on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,21 @@
-const submit = document.getElementbyID("submit");
+const submit = document.getElementById("submit");
 window.onload = () => {
-    console.log("hello:")
+
+console.log("hello:")
+
 //tocheck if the letters are 5 letters
 function myFunction() {
     // Get the value of the input field with id="submit"
     let x = document.getElementById("submit").value;
     // If x is 5
     let text;
-    if ( x = 5 ) {
+    if ( x > 5 ) {
       text = "Input not valid";
     } else {
       text = "Input OK";
     }
     document.getElementById("text").innerHTML = text;
   }
+   submit.addEventListener('click',myFunction);
+}
 
-//to check if the input is filled with the name
-function validate() {
-    if (document.myForm.name.value == "") {
-        alert("Enter a name");
-        document.myForm.name.focus();
-        return false;
-    }
-    if (!/^[a-zA-Z]*$/g.test(document.myForm.name.value)) {
-        alert("Invalid characters");
-        document.myForm.name.focus();
-        return false;
-    }
-}
-   submit.addEventListener('click',validate);
-}
