e66c04c654aac5d4c2332a10bd87fe5c4113ea08
FatimaKabalan <kabalan.fatima01@gmail.com>
2022-09-12 23:26:22 +0300

    adding the javascript function that checks the first name

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const submit = document.getElementbyID("submit");
+
+
+
+function validate() {
+    if (document.myForm.name.value == "") {
+        alert("Enter a name");
+        document.myForm.name.focus();
+        return false;
+    }
+    if (!/^[a-zA-Z]*$/g.test(document.myForm.name.value)) {
+        alert("Invalid characters");
+        document.myForm.name.focus();
+        return false;
+    }
+}
+
