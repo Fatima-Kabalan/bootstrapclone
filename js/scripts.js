d93e7a5d3b616774a6114795a30d4bdfa0c51565
FatimaKabalan <kabalan.fatima01@gmail.com>
2022-09-12 23:33:49 +0300

    adding a function that checks if there is 5 letters in the name

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 const submit = document.getElementbyID("submit");
 
 
+//tocheck if the letters are 5 letters
+function myFunction() {
+    // Get the value of the input field with id="submit"
+    let x = document.getElementById("submit").value;
+    // If x is 5
+    let text;
+    if ( x = 5 ) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("text").innerHTML = text;
+  }
 
+//to check if the input is filled with the name
 function validate() {
     if (document.myForm.name.value == "") {
         alert("Enter a name");
